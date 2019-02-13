1aaafa1272b63505679ede1e9f97e3dd6c86a146
FelixWeichselgartner <f.wei3012@gmail.com>
2019-02-13 22:25:59 +0100

    further programming

--- src/Snake.java
+++ src/Snake.java
@@ -40,14 +40,12 @@ public class Snake extends JPanel {
         board[y][x].setDirection(direction);
         board[y][x].setThisX(x);
         board[y][x].setThisY(y);
-        if (px != -1 && py != -1) {
-            board[y][x].setNextX(px);
-            board[y][x].setNextY(py);
-        }
+        board[y][x].setNextX(px);
+        board[y][x].setNextY(py);
     }
 
-    public void setHead(int x, int y) {
-        setNodeParameters(x, y, -1, -1, head, Node.right);
+    public void setHead(int x, int y, int px, int py) {
+        setNodeParameters(x, y, px, py, head, Node.right);
     }
 
     public void setBody(int x, int y, int px, int py) {
@@ -62,10 +60,10 @@ public class Snake extends JPanel {
         int headX = 10, headY = 10;
         this.headPositionX = headX;
         this.headPositionY = headY;
-        setHead(headX, headY);
+        setHead(headX, headY, headX - 1, headY);
         for (int i = 1; i < 4; i++) {
             if (i != 3) {
-                setBody(headX - i, headY, headX, headY - i + 1);
+                setBody(headX - i, headY, headX - i - 1, headY);
             } else {
                 setBody(headX - i, headY, -1, -1);
             }
@@ -87,7 +85,7 @@ public class Snake extends JPanel {
         this.GameState = 0;
         initBoard();
         initSnake();
-        //setWalls();
+        setWalls();
     }
 
     boolean collision(int x, int y) {
@@ -97,33 +95,69 @@ public class Snake extends JPanel {
         return false;
     }
 
-    public void moveSnake(int x, int y) {
-        int newX, newY;
-        switch (board[y][x].getDirection()) {
+    public boolean moveNode(int x, int y) {
+        char direction = board[y][x].getDirection();
+        switch (direction) {
+            case Node.noDirection:
+                break;
             case Node.right:
+                if (collision(x + 1, y)) {
+                    System.out.println("1");
+                    return false;
+                }
                 board[y][x + 1] = board[y][x];
                 break;
             case Node.left:
+                if (collision(x - 1, y)) {
+                    System.out.println("2");
+                    return false;
+                }
                 board[y][x - 1] = board[y][x];
                 break;
             case Node.up:
+                if (collision(x, y - 1)) {
+                    System.out.println("3");
+                    return false;
+                }
                 board[y - 1][x] = board[y][x];
                 break;
             case Node.down:
+                if (collision(x, y + 1)) {
+                    System.out.println("4");
+                    return false;
+                }
                 board[y + 1][x] = board[y][x];
                 break;
-            case Node.noDirection:
-                break;
         }
+        return true;
     }
 
-    public void moveDirection() {
+    public boolean moveDirection() {
         int x = this.headPositionX, y = this.headPositionY;
-        do {
+        int tempx, tempy;
+        int count = 0;
+        while (board[y][x].getNextX() != -1 || board[y][x].getNextY() != -1) {
+            System.out.println("moved!");
+            System.out.println("x: " + x + " y: " + y);
+            System.out.println("nextx: " + board[y][x].getNextX() + " nexty: " + board[y][x].getNextY());
+            if (!moveNode(x, y) && count == 0) {
+                return false;
+            }
             board[board[y][x].getNextX()][board[y][x].getNextY()].setDirection(board[y][x].getDirection());
-            //moveDirection(x, y);
-        } while (board[y][x].getNextX() != -1 && board[y][x].getNextY() != -1);
-
+            tempx = board[y][x].getNextX();
+            tempy = board[y][x].getNextY();
+            if (tempx == -1 && tempy == -1) {
+                board[tempy][tempx] = new Node();
+                return false;
+            }
+            x = tempx;
+            y = tempy;
+            count++;
+            if (count == 10) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -177,7 +211,6 @@ public class Snake extends JPanel {
         frame.addKeyListener(new KeyListener() {
             public void keyTyped(KeyEvent ke) {
                 char key = ke.getKeyChar();
-                //System.out.println(ke.getKeyChar());
                 System.out.println(key);
                 switch (key) {
                     case 'a':
@@ -216,16 +249,14 @@ public class Snake extends JPanel {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            moveDirection();
+
+            if (!moveDirection()) {
+                System.out.println("game over");
+                return;
+            }
 
             updateWindow(frame);
-
-            if (collision(headPositionX, headPositionY)) {
-                System.out.println("There was a collision!");
-                break;
-            }
         }
-
 
 
     }
@@ -233,8 +264,8 @@ public class Snake extends JPanel {
     public static void main(String[] args) {
         Snake newGame = new Snake();
         JFrame frame = new JFrame("Snake");
-        frame.setSize(newGame.width * newGame.getPixels(), newGame.height * newGame.getPixels());
-        frame.setResizable(false);
+        frame.setSize((newGame.width + 1) * newGame.getPixels(), (newGame.height + 3) * newGame.getPixels());
+        frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(newGame);
         frame.setVisible(true);
